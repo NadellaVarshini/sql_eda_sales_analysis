99334be53b832fdf71e181196c3e6dc406d4db39
NadellaVarshini <161931446+NadellaVarshini@users.noreply.github.com>
2026-02-16 19:49:20 +0530

    Update ChangeOverTime.sql

--- sql_scripts/ChangeOverTime.sql
+++ sql_scripts/ChangeOverTime.sql
@@ -1,5 +1,13 @@
---change over time
+--change over time analysis
 --measure by date dimension
+/*Purpose:
+    - To track trends, growth, and changes in key metrics over time.
+    - For time-series analysis and identifying seasonality.
+    - To measure growth or decline over specific periods.
+
+SQL Functions Used:
+    - Date Functions: DATEPART(), DATETRUNC(), FORMAT()
+    - Aggregate Functions: SUM(), COUNT(), AVG()*/
 
 select DATETRUNC(month,order_date) as order_date,
 sum(sales_amount) as total_sales,
@@ -8,4 +16,5 @@ sum(quantity) as total_quantity
 FROM gold.fact_sales
 where order_date is NOT NULL
 group by DATETRUNC(month,order_date)
+
 order by DATETRUNC(month,order_date);
